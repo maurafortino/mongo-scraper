88b6d20e5ebcac64d632ea8831835a11f3c9a73d
maurafortino <maurafortino@gmail.com>
2019-12-17 13:41:47 -0500

    trying to make this work

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/heroku_htsr70w8";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
 
